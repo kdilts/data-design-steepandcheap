025b35afecb49305b42b50403c0d7170792f6f02
Kevin Dilts <kdilts13@gmail.com>
2016-10-12 13:20:51 -0600

    added content to use case, interactions, and persona

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>Data Design Project Phase 0</title>
+		<link href="stylesheet.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
 		<header>
@@ -12,19 +13,39 @@
 
 			<!-- Persona -->
 			<section>
-				<h2>Persona</h2>
-				<p></p>
+				<h2 class="subheading">Persona</h2>
+				<ul>
+					<li><strong>Name:</strong> Kevin</li>
+					<li><strong>Age:</strong> 25</li>
+					<li><strong>Job:</strong> Web Developer in training. He spends most of his time learning about HTML, CSS, PHP, mySQL, and Javascript.</li>
+					<li><strong>Technology:</strong> PC Loyalist. Uses a desktop with Windows 10 and a big screen TV at home. As well as a Windows 10 laptop for traveling.</li>
+					<li><strong>Behavior:</strong> Kevin is a climbing enthusiast who compulsively checks and rechecks the website for good deals on climbing gear. Also he enjoys browsing through products just to learn their features and decide if they will be added to his mental wishlist.</li>
+					<li><strong>Goals:</strong> Kevin would like to build a collection of climbing gear for cheap. Also, he wants to learn everything he can about climbing, and part of that is gaining familiarity with all the different brands and products out there.</li>
+					<li><strong>Frustrations:</strong> REI is one of the only local places to buy gear, and they are very expensive.</li>
+				</ul>
 			</section>
 
 			<!-- Use Case -->
 			<section>
-				<h2>Use Case</h2>
-				<p></p>
+				<h3 class="subheading">Use Case</h3>
+			</section>
+
+			<!-- Interaction Flow -->
+			<section>
+				<h4 class="subheading">Interaction Flow</h4>
+				<ol>
+					<li>Navigate to steepandcheap.com</li>
+					<li>Look at current steal</li>
+					<li>Don't buy it because it's a shirt</li>
+					<li>Search "climb"</li>
+					<li>Set category to climb, and filter by >30% off</li>
+					<li>Scroll through list of items looking for anything of interest</li>
+				</ol>
 			</section>
 
 			<!-- Entities -->
 			<section>
-				<h2>Entities</h2>
+				<h5 class="subheading">Entities</h5>
 				<p></p>
 			</section>
 		</main>
