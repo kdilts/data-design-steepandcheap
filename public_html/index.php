643eb43964a932c6868c3b039ae4abfaeefcbfe9
Kevin Dilts <kdilts13@gmail.com>
2016-10-12 10:59:19 -0600

    created basic layout for index.php

---
+++ public_html/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Data Design Project Phase 0</title>
+	</head>
+	<body>
+		<header>
+			<h1>Data Design Project Phase 0</h1>
+		</header>
+		<main>
+
+			<!-- Persona -->
+			<section>
+				<h2>Persona</h2>
+				<p></p>
+			</section>
+
+			<!-- Use Case -->
+			<section>
+				<h2>Use Case</h2>
+				<p></p>
+			</section>
+
+			<!-- Entities -->
+			<section>
+				<h2>Entities</h2>
+				<p></p>
+			</section>
+		</main>
+	</body>
+</html>
