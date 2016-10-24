717b8c06140022a40f7ea946efa5a42508a1f1ce
Kevin Dilts <kdilts13@gmail.com>
2016-10-24 10:28:05 -0600

    finished getUserByHash

--- php/User.php
+++ php/User.php
@@ -393,7 +393,46 @@ class User implements \JsonSerializable {
 		return($users);
 	}
 
-	public static function getUserByHash(\PDO $pdo, string $userHash){}
+	public static function getUserByHash(\PDO $pdo, string $userHash){
+		// sanitize userHash before searching
+		$userHash = trim($userHash);
+		$userHash = filter_var($userHash, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($userHash) === true){
+			throw (new \PDOException("userHash is invalid"));
+		}
+
+		// create query template
+		$query = "SELECT userId, userName, userHash, userSalt, userAddress, userEmail FROM user WHERE userHash LIKE :userHash";
+		$statement = $pdo->prepare($query);
+
+		// bind userHash to placeholder in template
+		$userHash = "%$userHash%";
+		$parameters = ["userHash" => $userHash];
+		$statement->execute($parameters);
+
+		// build array of users
+		$users = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$user = new User(
+					$row["userId"],
+					$row["userName"],
+					$row["userHash"],
+					$row["userSalt"],
+					$row["userAddress"],
+					$row["userEmail"]
+				);
+				$users[$users->key()] = $user;
+				$users->next();
+			} catch(\Exception $exception) {
+				// if row couldn't be converted rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+
+		return($users);
+	}
 
 	public static function getUserBySalt(\PDO $pdo, string $userSalt){
 		// sanitize userSalt before searching
